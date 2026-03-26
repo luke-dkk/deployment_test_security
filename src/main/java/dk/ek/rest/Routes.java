5337e20c78b9528954c64fb39fe4a102bf51a105
luke-dkk <luke_persson@yahoo.dk>
2026-03-26 16:59:39 +0100

    updated to test watchtower

--- src/main/java/dk/ek/rest/Routes.java
+++ src/main/java/dk/ek/rest/Routes.java
@@ -30,7 +30,7 @@ public class Routes {
         return switch (resourceName.toLowerCase()) {
             case "msg" -> () -> path("msg", () -> {
             ObjectNode on = objectMapper.createObjectNode();
-            on.put("msg", "Hello World from watchtower - I am now online");
+            on.put("msg", "Hello World from watchtower - I am now online at Martins");
                 get("hello", ctx -> ctx.json(on));
                 post("echo", ctx -> ctx.result(ctx.body()));
             });
